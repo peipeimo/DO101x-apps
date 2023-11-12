fb0b9b36959ab7c49d2c9fb98a0965d1a91c1302
peipeimo <peipeimo@gmail.com>
2023-11-12 02:15:23 -0500

    update to mars

--- version/app.js
+++ version/app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 app = express();
 
-var response;
+// var response;
 
-app.get('/', function (req, res) {
+// app.get('/', function (req, res) {
 
-    response = 'This is version 3 of the app.' + '\n';
+//     response = 'This is version 3 of the app.' + '\n';
 
-    //send the response to the client
-    res.send(response);
+//     //send the response to the client
+//     res.send(response);
 
+// });
+
+app.get('/mars', function (req, res) {
+  res.send('Hello Mars!\n');
 });
 
 app.listen(8080, function () {
